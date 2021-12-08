630098c0cec6ea90bb8f5c0010e5eb06c717c050
KolominD <bogkolomind@gmail.com>
2021-12-08 19:36:12 +0300

    initial commit< but with ci

---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,84 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.*;
+import ru.netology.page.DashboardPage;
+
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+    public DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setUp() {
+        Configuration.headless = true;
+        open("http://localhost:9999", login.class);
+        val login = new login();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = login.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        dashboardPage = verificationPage.validVerify(verificationCode);
+
+    }
+
+    @Test
+    public void transferMoneyFirstToSecond() {
+        int amount = 700;
+        var firstCardData = DataHelper.CardData.getFirstCardData();
+        var secondCardData = DataHelper.CardData.getSecondCardData();
+        int firstCardBalanceBeforeTransfer = dashboardPage.getFirstCardCurrentBalance();
+        int secondCardCurrentBalanceBeforeTransfer = dashboardPage.getSecondCardCurrentBalance();
+        val moneyTransferPage = dashboardPage.secondCard();
+        moneyTransferPage.moneyTransfer(firstCardData, amount);
+        int firstCardBalanceAfterTransfer = DataHelper.balanceCardTransferFrom(firstCardBalanceBeforeTransfer, amount);
+        int secondCArdBalanceAfterTransfer = DataHelper.balanceCardTransferTo(secondCardCurrentBalanceBeforeTransfer, amount);
+        int firstCardNewBalance = dashboardPage.getFirstCardCurrentBalance();
+        int secondCArdNewBalance = dashboardPage.getSecondCardCurrentBalance();
+        assertEquals(firstCardBalanceAfterTransfer, firstCardNewBalance);
+        assertEquals(secondCArdBalanceAfterTransfer, secondCArdNewBalance);
+
+    }
+
+    @Test
+    public void transferZeroFromFirstToSecond() {
+        int amount = 0;
+        var firstCardData = DataHelper.CardData.getFirstCardData();
+        var secondCardData = DataHelper.CardData.getSecondCardData();
+        int firstCardBalanceBeforeTransfer = dashboardPage.getFirstCardCurrentBalance();
+        int secondCardCurrentBalanceBeforeTransfer = dashboardPage.getSecondCardCurrentBalance();
+        val moneyTransferPage = dashboardPage.secondCard();
+        moneyTransferPage.moneyTransfer(firstCardData, amount);
+        int firstCardBalanceAfterTransfer = DataHelper.balanceCardTransferFrom(firstCardBalanceBeforeTransfer, amount);
+        int secondCArdBalanceAfterTransfer = DataHelper.balanceCardTransferTo(secondCardCurrentBalanceBeforeTransfer, amount);
+        int firstCardNewBalance = dashboardPage.getFirstCardCurrentBalance();
+        int secondCArdNewBalance = dashboardPage.getSecondCardCurrentBalance();
+        assertEquals(firstCardBalanceAfterTransfer, firstCardNewBalance);
+        assertEquals(secondCArdBalanceAfterTransfer, secondCArdNewBalance);
+
+    }
+
+    @Test
+    public void transferFromFirstToGoUnderThanLimit() {
+        int amount = 30000;
+        var firstCardData = DataHelper.CardData.getFirstCardData();
+        var secondCardData = DataHelper.CardData.getSecondCardData();
+        int firstCardBalanceBeforeTransfer = dashboardPage.getFirstCardCurrentBalance();
+        int secondCardCurrentBalanceBeforeTransfer = dashboardPage.getSecondCardCurrentBalance();
+        val moneyTransferPage = dashboardPage.secondCard();
+        moneyTransferPage.moneyTransfer(firstCardData, amount);
+        $(withText("Ошибка. На балансе недостаточно средств")).shouldBe(Condition.visible);
+
+    }
+
+
+}
+
